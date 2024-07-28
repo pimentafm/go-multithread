30943e3753dcb89523d53359ff17c027469275a8
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-28 00:32:43 -0300

    Initial Commit

---
+++ cmd/app/main.go
@@ -0,0 +1,18 @@
+package main
+
+type Address struct {
+	CEP         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
+	IBGE        string `json:"ibge"`
+	GIA         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	SIAFI       string `json:"siafi"`
+}
+
+func main() {
+
+}
